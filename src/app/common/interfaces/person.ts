1bee605cc057fa6964e3e9e11aadeeebd6c34d2c
LincDi <88975046+LincDi@users.noreply.github.com>
2021-12-27 15:02:28 +0300

    modify person.ts

--- src/app/common/interfaces/person.ts
+++ src/app/common/interfaces/person.ts
@@ -1,1 +1,10 @@
-export interface Person {}
+export interface Person {
+  name: string;
+  lastName: string;
+  nickName: string;
+  avatar: string;
+  status: boolean; //online or offline
+  subscribe: boolean; //premium user
+  showName: boolean; //show name and last name or show nick name
+  about?: string; //abaut of person
+}
