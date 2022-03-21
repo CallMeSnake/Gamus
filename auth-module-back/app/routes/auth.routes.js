e9346b31efa1d978b4e0da4723954a5f8dfe1f30
Valentine Tury <mrjf009@gmail.com>
2022-03-21 17:08:01 +0100

    Final fixes to the Back end

--- auth-module-back/app/routes/auth.routes.js
+++ auth-module-back/app/routes/auth.routes.js
@@ -1,22 +1,14 @@
-const { authJwt } = require("../middleware");
-const controller = require("../controllers/user.controller");
-
+const { verifySignUp } = require('../middleware');
+const controller = require('../controllers/auth.controller');
 module.exports = function (app) {
   app.use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
+    res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
     next();
   });
-
-  app.get("/api/test/all", controller.allAccess);
-
-  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
-
-  app.get(
-    "/api/test/admin",
-    [authJwt.verifyToken, authJwt.isAdmin],
-    controller.adminBoard
+  app.post(
+    '/api/auth/signup',
+    [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted],
+    controller.signup
   );
+  app.post('/api/auth/signin', controller.signin);
 };
--- auth-module-back/server.js
+++ auth-module-back/server.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+var bodyParser = require('body-parser');
 
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: 'http://localhost:8081',
 };
 
 app.use(cors(corsOptions));
@@ -13,17 +14,20 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./app/models");
+const db = require('./app/models');
 const Role = db.role;
 
 db.sequelize.sync();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Gamus application." });
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Gamus application.' });
 });
 
-require("./app/routes/auth.routes")(app);
-require("./app/routes/user.routes")(app);
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
@@ -33,11 +37,11 @@ app.listen(PORT, () => {
 function initial() {
   Role.create({
     id: 1,
-    name: "user",
+    name: 'user',
   });
 
   Role.create({
     id: 2,
-    name: "admin",
+    name: 'admin',
   });
 }
