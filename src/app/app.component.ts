2dc8383b27b1e3d562e816655c7bcaa3f3198338
Valentine Tury <mrjf009@gmail.com>
2022-05-19 19:12:00 +0200

    Remastered sidebar with Sidenav Angular | Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,6 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.store.dispatch(getCurrentUserAction());
   }
+  events: string[] = [];
+  opened!: boolean;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -14,6 +16,7 @@ import { SidebarModule } from './shared/modules/sidebar/sidebar.module';
 import { TopBarModule } from './shared/modules/top-bar/top-bar.module';
 import { AuthInterceptor } from './shared/services/auth-interceptor.service';
 import { PersistanceService } from './shared/services/persistance.service';
+import { MaterialExampleModule } from '../material.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -28,6 +31,11 @@ import { PersistanceService } from './shared/services/persistance.service';
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     TopBarModule,
     SidebarModule,
+
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
   ],
   providers: [
     PersistanceService,
