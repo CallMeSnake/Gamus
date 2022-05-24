66054fdf4053222a4430844f651777bac7112f82
Valentine Tury <mrjf009@gmail.com>
2022-05-24 10:47:34 +0200

    Added "RouterModule"

--- src/app/shared/modules/homepage/homepage.module.ts
+++ src/app/shared/modules/homepage/homepage.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HomepageComponent } from './components/homepage/homepage.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+];
+
 @NgModule({
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, RouterModule.forChild(routes)],
   declarations: [HomepageComponent],
   exports: [HomepageComponent],
 })
