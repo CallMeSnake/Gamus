76242687362659fb5f8718cad874b8bd2443740c
Valentine Tury <mrjf009@gmail.com>
2022-05-24 13:36:06 +0200

    Url moved to variable

--- src/app/shared/modules/homepage/components/homepage/homepage.component.ts
+++ src/app/shared/modules/homepage/components/homepage/homepage.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../../../environments/environment';
 
 @Component({
   selector: 'homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  homePage = environment.homePage;
+}
